6e6050352140c507f98d5ce5c2d1e434dc2c0c15
Lunes313 <99741938+Lunes313@users.noreply.github.com>
2024-03-20 17:44:34 -0500

    Cambio de vista del inicio
    Se cambio la visualización del formulario de inicio de sesión

--- inicio/login.php
+++ inicio/login.php
@@ -1,6 +1,29 @@
 <?php
 session_start();
- ?>
+
+if (isset($_POST['ingresar'])) {
+    $usem = $_POST['usem'];
+    $password = $_POST['password'];
+    $conexion = new mysqli("localhost", "root", "", "base_proyecto");
+    $sql2 = "SELECT * FROM base_usuario WHERE nombre='$usem' AND contrasena='$password'";
+    $resultado = $conexion->query($sql2);
+    $dato = $resultado->fetch_assoc();
+    $ejecutar2 = mysqli_query($conexion, $sql2);
+    if ($dato) {
+        $_SESSION['id_usuario'] = $dato['ID'];
+        $_SESSION['usuario'] = $dato['nombre'];
+        if ($dato['perfil'] == 'estudiante') {
+            header("location:../otro/estudiante.php");
+        } else if ($dato['perfil'] == 'hogar') {
+            header("location:../otro/hogar.php");
+        }
+
+    } else {
+        echo "<script>alert('Usuario o contraseña incorrectos');</script>";
+        echo "<script>window.location.href='login.php';</script>";
+    }
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,6 +34,49 @@ session_start();
     <link rel="stylesheet" href="stylelogin.css">
 </head>
 <body>
+<div id="container">
+    <!-- Cover Box -->
+    <div id="cover">
+        <!-- Sign Up Section -->
+        <h1 class="sign-up">Hello, Friend!</h1>
+        <p class="sign-up">Enter your personal details<br> and start a journey with us</p>
+        <a class="button sign-up" href="#cover">Sign Up</a>
+        <!-- Sign In Section -->
+        <h1 class="sign-in">Welcome Back!</h1>
+        <p class="sign-in">To keep connected with us please<br> login with your personal info</p>
+        <br>
+        <a class="button sub sign-in" href="#">Sign In</a>
+    </div>
+    <!-- Login Box -->
+    <div id="login">
+        <h1>Sign In</h1>
+        <a href="#"><img class="social-login" src="https://image.flaticon.com/icons/png/128/59/59439.png"></a>
+        <a href="#"><img class="social-login" src="https://image.flaticon.com/icons/png/128/49/49026.png"></a>
+        <a href="#"><img class="social-login" src="https://image.flaticon.com/icons/png/128/34/34227.png"></a>
+        <p>or use your email account:</p>
+        <form>
+            <input type="email" placeholder="Email" autocomplete="off"><br>
+            <input type="password" placeholder="Password" autocomplete="off"><br>
+            <a id="forgot-pass" href="#">Forgot your password?</a><br>
+            <input class="submit-btn" type="submit" value="Sign In">
+        </form>
+    </div>
+    <!-- Register Box -->
+    <div id="register">
+        <h1>Create Account</h1>
+        <a href="#"><img class="social-login" src="https://image.flaticon.com/icons/png/128/59/59439.png"></a>
+        <a href="#"><img class="social-login" src="https://image.flaticon.com/icons/png/128/49/49026.png"></a>
+        <a href="#"><img class="social-login" src="https://image.flaticon.com/icons/png/128/34/34227.png"></a>
+        <p>or use your email for registration:</p>
+        <form>
+            <input type="text" placeholder="Name" autocomplete="off"><br>
+            <input type="email" placeholder="Email" autocomplete="off"><br>
+            <input type="password" placeholder="Password" autocomplete="off"><br>
+            <input class="submit-btn" type="submit" value="Sign Up">
+        </form>
+    </div>
+</div>
+
 
 <div class="contenedor">
 <div class="contenedorcolor">
@@ -30,35 +96,7 @@ session_start();
         <input type="submit" class="boton" name="ingresar" ><center><p  class="pb"></p></center></div>
 
         </form>
-    <?php
-	
-    if (isset($_POST['ingresar'])) {
-        $usem=$_POST['usem'];
-        $password=$_POST['password'];
-        $conexion=new mysqli("localhost","root","","base_proyecto");
-		$sql2= "SELECT * FROM base_usuario WHERE nombre='$usem' AND contrasena='$password'";
-		$resultado=$conexion->query($sql2);
-		$dato=$resultado->fetch_assoc();
-		$ejecutar2=mysqli_query($conexion, $sql2);
-        if($dato)
-			{
-                $_SESSION['id_usuario'] = $dato['ID'];
-                $_SESSION['usuario']=$dato['nombre'];
-                if($dato['perfil']=='estudiante'){
-				header("location:../otro/estudiante.php");
-                }
-                else if($dato['perfil']=='hogar'){
-                    header("location:../otro/hogar.php");
-                }
 
-			}
-            else{
-                echo "<script>alert('Usuario o contraseña incorrectos');</script>";
-                echo "<script>window.location.href='login.php';</script>";
-            }
-}
-
-    ?>
 </div>
     </div>
 
