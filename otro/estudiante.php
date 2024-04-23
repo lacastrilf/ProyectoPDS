b4bbcbb1ffcffb394d4d4fbd9561601dcf12fcdf
lacastrilf <159634453+lacastrilf@users.noreply.github.com>
2024-04-23 18:00:37 -0500

    Gastos totales en perfil estudiante

--- otro/estudiante.php
+++ otro/estudiante.php
@@ -12,6 +12,11 @@ $sqlSuma = "SELECT SUM(alimentacion + colchon + ocio + servicios + transporte + 
 $resultadoSuma = $conexion->query($sqlSuma);
 $sumaTotal = $resultadoSuma->fetch_assoc()['suma_total'];
 
+//Obtener la suma total de los gastos
+$sqlSumaG = "SELECT SUM(transporte+alimentacion+ocio) AS suma_total FROM diagramagastosestudiante WHERE idUsuario='$idUsuario'";
+$resultadoSumaG= $conexion->query($sqlSumaG);
+$sumaTotalG = $resultadoSumaG->fetch_assoc()['suma_total'];
+
 //Creación de fila de la tabla diagrama Costos de estudiantes
 $sqlSeleccion= "SELECT * FROM diagramagastosestudiante WHERE idUsuario = '$idUsuario'";
 $resultado = $conexion->query($sqlSeleccion);
@@ -36,6 +41,17 @@ if(isset($_POST['enviarPendiente'])){
   $ejecutar3 = mysqli_query($conexion, $sqlInsertPendiente);
     header("Location: {$_SERVER['PHP_SELF']}");
 }
+
+//Creación de fila de la tabla promedio semanas
+$sqlSeleccionSemanas= "SELECT * FROM semanas WHERE idUsuario = '$idUsuario'";
+$resultado = $conexion->query($sqlSeleccionSemanas);
+if ($resultado->num_rows == 0) {
+    $sqlIngresarSemanas="INSERT INTO semanas VALUES ('null', '$idUsuario', '0', '0', '0', '0','0')";
+    $ejecutar4 = mysqli_query($conexion, $sqlIngresarSemanas);
+}
+
+
+
 
 ?>
 
@@ -360,21 +376,23 @@ if(isset($_POST['enviarPendiente'])){
                   <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
                   <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
                     <li class="dropdown-header text-start">
-                      <h6>Filter</h6>
+                      <h6>Actualizar</h6>
                     </li>
-                    <li><a class="dropdown-item" href="#">Today</a></li>
-                    <li><a class="dropdown-item" href="#">This Month</a></li>
-                    <li><a class="dropdown-item" href="#">This Year</a></li>
+                    <li>
+                      <form action="estudiante.php" method="POST">
+                      <input type="hidden" value="<?php echo (isset($sumaTotalG) ? $sumaTotalG : 0); ?>" name="presupuestoProm">
+                      <input type="submit" class="dropdown-item" href="#" name="actualizarSemanas" value="Nueva semana">
+                    </form></li>
                   </ul>
                 </div>
                 <div class="card-body">
-                  <h5 class="card-title">Sales <span>| Today</span></h5>
+                  <h5 class="card-title">Gastos <span>| Semanales</span></h5>
                   <div class="d-flex align-items-center">
                     <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
                       <i class="bi bi-cart"></i>
                     </div>
                     <div class="ps-3">
-                      <h6>145</h6>
+                      <h6>$<?php echo($sumaTotalG) ?></h6>
                       <span class="text-success small pt-1 fw-bold">12%</span> <span class="text-muted small pt-2 ps-1">increase</span>
                     </div>
                   </div>
@@ -388,11 +406,9 @@ if(isset($_POST['enviarPendiente'])){
                   <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
                   <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
                     <li class="dropdown-header text-start">
-                      <h6>Filter</h6>
+                      <h6>Actualizar</h6>
                     </li>
-                    <li><a class="dropdown-item" href="#">Today</a></li>
-                    <li><a class="dropdown-item" href="#">This Month</a></li>
-                    <li><a class="dropdown-item" href="#">This Year</a></li>
+                   
                   </ul>
                 </div>
                 <div class="card-body">
